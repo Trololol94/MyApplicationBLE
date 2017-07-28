4ae8beee7ffe3d63b90864e039b90e213bfb5dc3
Trololol94 <mali3355>
2017-07-28 15:08:12 +0200

    manifest normalement prêt

--- app/src/main/java/com/example/adalexan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/adalexan/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.adalexan.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private BluetoothAdapter mBluetoothAdapter;
+    // Initializes Bluetooth adapter.
+    final BluetoothManager bluetoothManager =
+            (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+    mBluetoothAdapter = bluetoothManager.getAdapter();
+
+    // Ensures Bluetooth is available on the device and it is enabled. If not,
+    // displays a dialog requesting user permission to enable Bluetooth.
+    if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
     }
 }
